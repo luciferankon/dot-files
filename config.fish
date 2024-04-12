adc0d75ac36c51d5a908e700e49af6a37da83a25
Ankon Chakraborty <44019225+luciferankon@users.noreply.github.com>
2024-04-12 15:09:02 +1000

    Updated dotfiles

---
+++ config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2023-11-12 23:56:04
+set PATH $PATH /Users/luciferankon/.local/bin
+pyenv init - | source
